410ccacbb5f769f2a3f5b4dae298d242d8bc3f04
Constantin Wolf <constantin.wolf@pwc.com>
2023-09-04 22:11:19 +0200

    select channel via commandline `-ndiChannel <channel Name>`

--- Assets/SourceSelector.cs
+++ Assets/SourceSelector.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System.Linq;
+using System    ;
 using Klak.Ndi;
 
 namespace Nsm {
@@ -18,10 +19,16 @@ public sealed class SourceSelector : MonoBehaviour
     // three child objects only while it's opened.
     bool IsOpened => _dropdown.transform.childCount > 3;
 
-    void Start() => _receiver = GetComponent<NdiReceiver>();
+    void Start() {
+        _receiver = GetComponent<NdiReceiver>();
+
+        // check for available arguments
+        readArguments();
+    }
 
     void Update()
     {
+        // readArguments();
         // Do nothing if the menu is opened.
         if (IsOpened) return;
 
@@ -36,6 +43,8 @@ public sealed class SourceSelector : MonoBehaviour
         {
             index = _sourceNames.Count;
             _sourceNames.Add(_receiver.ndiName);
+
+            readArguments();
         }
 
         // Disable the callback while updating the menu options.
@@ -45,6 +54,65 @@ public sealed class SourceSelector : MonoBehaviour
         _dropdown.ClearOptions();
         _dropdown.AddOptions(_sourceNames);
         _dropdown.value = index;
+        _dropdown.RefreshShownValue();
+
+        // Resume the callback.
+        _disableCallback = false;
+    }
+
+    public void readArguments() {
+        string[] args = System.Environment.GetCommandLineArgs ();
+        string input = "";
+        bool takeNextArgumentAsValue = false;
+        for (int i = 0; i < args.Length; i++) {
+            // Debug.LogError ("ARG " + i + ": " + args );
+            for (int subi = 0; subi < args.Length; subi++) {
+
+                // Debug.Log ("'" + args[subi] +"'");
+
+                if(String.Compare(args[subi],"-ndiChannel")==0) {
+                    takeNextArgumentAsValue = true;
+                    continue;
+                }
+
+                if(takeNextArgumentAsValue) {
+                    //  Debug.Log ("Argument value: " + args[subi] );
+                     takeNextArgumentAsValue = false;
+
+
+                    string sourceToSelect = args[subi];
+                    // String sourceToSelect = new String("DE_C02D66WXMD6T (Scan Converter)");
+                    // String sourceToSelect = new String("DE_C02D66WXMD6T (Test Patterns)");
+
+                     updateDropDown(sourceToSelect);
+                }
+            }
+        }
+    }
+
+    public void updateDropDown(String valueToSet) {
+        // Currect selection
+        var index = _sourceNames.IndexOf(valueToSet);
+
+        // Append the current name to the list if it's not found.
+        if (index < 0)
+        {
+            Debug.Log("❌ NDI channel not found: " + valueToSet);
+            return;
+        } else {
+
+            Debug.Log("✅ NDI channel set successfully: " + valueToSet + " index:" + index);
+        }
+
+        // Disable the callback while updating the menu options.
+        _disableCallback = true;
+
+        _dropdown.value = index;
+        _receiver.ndiName = _sourceNames[index];
+
+        var go = _dropdown.gameObject;
+        go.SetActive(false);
+
         _dropdown.RefreshShownValue();
 
         // Resume the callback.
